061366154a30b00df4594b7a399df2be8447c3c5
ramchalamkr <ramchalamkr@gmail.com>
2018-02-10 00:36:27 -0500

    Lonely Integer

---
+++ Bitmanipulation/LonelyInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+
+	static int Lonelyinteger(int[] a)
+	{
+		int res = 0;
+		foreach(int i in a)
+		{
+			res ^= i;
+		}
+		return res;
+	}
+
+	static void Main(String[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		string[] a_temp = Console.ReadLine().Split(' ');
+		int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+		int result = Lonelyinteger(a);
+		Console.WriteLine(result);
+	}
+}
